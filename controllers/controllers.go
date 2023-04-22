373b1640543493c752dd8347c9892fd52a4f02b0
rin <duggerseven@gmail.com>
2023-04-22 10:43:22 +0700

    update

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"ecommerce-project/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+	"net/http"
+	"time"
+)
 
 func HashPassword(password string) string {
 
@@ -18,8 +27,58 @@ func Login() gin.HandlerFunc {
 
 }
 
-func SignUp() gin.HandlerFunc {
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "This phone number is already in use."})
+			return
+		}
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.ObjectID{}
+		user.User_ID = user.ID.Hex()
+		token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refreshtoken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.AddressDetails = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+	}
 }
 
 func SearchProduct() gin.HandlerFunc {
